1fd3467b37bd015d2b1c6c4546a8c20d6424546e
ding <1262037115@qq.com>
2019-09-20 15:11:13 +0800

    test

--- api/src/main/java/api/Library.java
+++ api/src/main/java/api/Library.java
@@ -4,7 +4,11 @@
 package api;
 
 public class Library {
-    public boolean someLibraryMethod() {
-        return true;
-    }
+	public boolean someLibraryMethod() {
+		return true;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
 }
